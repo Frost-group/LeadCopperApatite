04a7d9ba83d1cc46b8a58882068b2c0b763a436b
Jarvist Moore Frost <jarvist@fakemail.fake>
2023-07-27 09:52:29 +0100

    Simple script to convert ASE traj to XDATCAR traj, with ASE.

---
+++ 0003-M3GNET-MD-ZORAC/convert_ase_traj_to_xdatcar.py
@@ -0,0 +1,11 @@
+import ase 
+import sys
+
+from ase.io import read, write
+from ase.io.vasp import write_vasp_xdatcar
+
+for f in sys.argv[1:]:
+    print(f + " => " + f"{f}.xdatcar")
+    t = ase.io.read(f, index=':')
+    ase.io.vasp.write_vasp_xdatcar(f"{f}.xdatcar", t, label=None)
+
