d3aa0cc685ce6c36ccdccbf18296c2fae120f257
Jarvist Moore Frost <jarvist@gmail.com>
2023-07-31 16:13:26 +0100

    Weekend run; 2x2x2 supercell 100ps CPU on Zorac

---
+++ 0003-M3GNET-MD-ZORAC/2x2x2_supercell/convert_ase_traj_to_xdatcar.py
@@ -0,0 +1,20 @@
+import ase 
+import sys
+
+from ase.io import read, write
+from ase.io.vasp import write_vasp_xdatcar
+from ase.io.netcdftrajectory import write_netcdftrajectory
+from ase.io.proteindatabank import write_proteindatabank
+
+for f in sys.argv[1:]:
+    print(f + " => " + f"{f}.netcdf")
+    t = ase.io.read(f, index=':')
+
+    # Trajectory formats
+    #ase.io.vasp.write_vasp_xdatcar(f"{f}.xdatcar", t, label=None)
+    ase.io.netcdftrajectory.write_netcdftrajectory(f"{f}.netcdf", t)
+    
+    # Single snapshots, sometimes required for atom type
+    ase.io.vasp.write_vasp(f"{f}.poscar", t[0])
+    ase.io.proteindatabank.write_proteindatabank(f"{f}.pdb", t[0])
+
